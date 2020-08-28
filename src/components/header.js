b7dd00e459183801839e8aac4e90e1220fac82cc
Lincon Brito <38511541+zlincon@users.noreply.github.com>
2020-08-28 16:48:54 -0300

    @

---
+++ src/components/header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';  
+
+import '../css/styles.css';
+
+class Header extends Component {
+    render() {
+        return (
+            <header>
+                <div className="logo">Logo</div>
+                <input  type="text"/>
+            </header>
+        )
+    }
+}
+
+export default Header;

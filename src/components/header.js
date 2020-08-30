62a8e73ff57a2b45f0f7903c919016a2d4f340a6
zlincon <38511541+zlincon@users.noreply.github.com>
2020-08-29 23:53:14 -0300

    Concluído react basics

--- src/components/header.js
+++ src/components/header.js
@@ -1,30 +1,17 @@
-import React, { Component } from 'react';  
+import React from 'react';  
 
 import '../css/styles.css';
 
-class Header extends Component {
-    state = {
-        keywords: ''
-    }
-    inputC = (event) => {
-        this.setState({
-            keywords: event.target.value
-        })
-    }
-    
-    render() {
-
+const Header = (props) => {
         return (
             <header>
                 <div className="logo">Logo</div>
                 <input  
                     type="text" 
-                    onChange={this.inputC}
+                    onChange={props.keywords}
                 />
-                <div>{this.state.keywords}</div>
             </header>
         )
-    }
 }
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -9,13 +9,32 @@ import NewsList from './components/news_list';
 
 class App extends Component{
   state = {
-    news:JSON
+    news:JSON,
+    filtered: []
   }
+
+  getKeyword = (event) => {
+    // console.log(event.target.value)
+    let keyword = event.target.value;
+    let filtered = this.state.news.filter((item) => {
+      return item.title.indexOf(keyword) > -1
+    });
+
+    this.setState({
+      // filtered: filtered
+      filtered
+    })
+
+    // console.log(filtered);
+  }
+
   render(){
+    let newsFiltered = this.state.filtered;
+    let newsWhole = this.state.news;
     return(
       <>
-        <Header/>
-        <NewsList news={this.state.news} donkey="Me">
+        <Header keywords={this.getKeyword}/>
+        <NewsList news={newsFiltered.length === 0 ? newsWhole : newsFiltered}>
           Hello dudes
         </NewsList>
       </>
